d8821abc6442e7414b232c692287d7c92a27acbe
mcepulis <109752570+mcepulis@users.noreply.github.com>
2024-03-05 17:46:24 +0200

    test4

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ import { externalWeb } from './pages/external.js';
 
 const app = express();
 const port = 4811;
-app.use(express.static("public"))
+
 app.get('/', (req, res) => res.send(homePage()));
 app.get('/about', (req, res) => res.send(aboutPage()));
 app.get('/contact-us', (req, res) => res.send(contactUsPage()));
