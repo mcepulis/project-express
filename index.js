629f67320b23ba4c5cafcdf1201ae0936bb010ae
mcepulis <109752570+mcepulis@users.noreply.github.com>
2024-03-05 15:53:12 +0200

    my tests

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ import { aboutPage } from './pages/about.js';
 import { contactUsPage } from './pages/contactUs.js';
 import { servicesPage } from './pages/services.js';
 import { page404 } from './pages/404.js';
+import { asmeninis } from './pages/asmeninis.js';
+import { externalWeb } from './pages/external.js';
 
 const app = express();
 const port = 4811;
@@ -14,6 +16,9 @@ app.get('/', (req, res) => res.send(homePage()));
 app.get('/about', (req, res) => res.send(aboutPage()));
 app.get('/contact-us', (req, res) => res.send(contactUsPage()));
 app.get('/services', (req, res) => res.send(servicesPage()));
+app.get('/asmeninis', (req, res) => res.send(asmeninis()));
+
+app.get('/externalWEB', (req, res) => res.send(externalWeb()));
 
 app.use((req, res, next) => {
     res.status(404).send(page404());
