02f7665b79bc20f5eb304d2703220343f648cad4
mcepulis <109752570+mcepulis@users.noreply.github.com>
2024-03-13 17:13:16 +0200

    commit ND 3/13/2024

--- api/index.js
+++ api/index.js
@@ -1,3 +1,4 @@
+import { timeToWords } from './timeToWords.js';
 import { Router } from "express";
 
 const apiRouter = Router();
@@ -21,5 +22,29 @@ apiRouter.get('/api/daugyba/:num1/:num2', (req, res) => {
 apiRouter.get('/api/dalyba/:num1/:num2', (req, res) => {
     res.send(`${Number(req.params.num1) / Number(req.params.num2)}`);
 });
+
+
+// Reikia priimti varda ir pavarde, ir graziname inicialus.
+// /api/abbr/chuck/norris => C.N.
+apiRouter.get('/api/abbr/:name/:surname', (req, res) => {
+    const name = req.params.name;
+    const surname = req.params.surname;
+    res.send(`${name[0]}.${surname[0]}`.toLocaleUpperCase());
+    
+});
+
+// Kreipiantis konkreciai i toli URL yra grazinamas laikas: hh:mm:ss
+apiRouter.get('/api/time', (req, res) => {
+    res.send(`${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}`);
+});
+
+// Kreipiantis konkreciai i toli URL yra grazinamas laikas tokiu formatu hh:mm:ss, bet visi skaiciai yra zodziai
+// pvz.: 10:57:14 => desimt valandu, penkiasdesimt septynios minutes, keturiolika sekundziu
+// pasistenkti apgalvoti visas galimas gramatikos situacijas
+apiRouter.get('/api/time-as-text', (req, res) => {
+    const timeAsText = timeToWords();
+    return res.send(timeAsText);
+});
+
 
 export { apiRouter };
---
+++ api/timeToWords.js
@@ -0,0 +1,59 @@
+function timeToWords() { 
+    let currentTime = new Date();
+    let hh = currentTime.getHours();
+    let mm = currentTime.getMinutes();
+    let ss = currentTime.getSeconds();
+
+    let words = [
+        "viena", "dvi", "trys", "keturios", "penkios", "šešios", "septynios", "aštuonios", "devynios", "dešimt", 
+        "vienuolika", "dvylika", "trylika", "keturiolika", "penkiolika", "šešiolika", "septyniolika", "aštuoniolika", 
+        "devyniolika", "dvidešimt", "trisdešimt", "keturiasdešimt", "penkiasdešimt"
+        ];
+  
+    let minutes = ""; 
+    let seconds = "";
+  
+    if (mm < 20) { 
+        minutes = words[mm - 1]; 
+    } else {
+        minutes = words[(17 + Math.floor(mm / 10))] + " " + words[(mm % 10) - 1]; 
+    }
+
+    if (ss < 20) { 
+        seconds = words[ss - 1];
+    } else {
+        seconds = words[(17 + Math.floor(ss / 10))] + " " + words[(ss % 10) - 1];
+    }
+    let wordHour = "";
+    let wordMinute = "";
+    let wordSecond = "";
+    if (hh == 1) {
+        wordHour = " valanda";
+    } else if (hh > 1 && hh < 10) {
+       wordHour = " valandos";
+    } else {
+        wordHour = " valandų";
+    }
+        
+    if (mm == 1 && mm == 21 && mm == 31 && mm == 41 && mm == 51) {
+         wordMinute = " minutė";
+    } else if (mm >= 10 && mm <= 20) {
+        wordMinute = " minučių";
+    } else {
+         wordMinute = " minutės";
+    }
+  
+
+    if (ss == 1 && ss == 21 && ss == 31 && ss == 41 && ss == 51) {
+         wordSecond = " sekundė";
+    } else if (ss >= 10 && ss <= 20) {
+         wordSecond = " sekundžių";
+    } else {
+         wordSecond = " sekundės";
+    }
+
+
+    return words[hh - 1] + wordHour + " " + minutes + wordMinute + " ir " + seconds + wordSecond; 
+} 
+
+export { timeToWords }
