26c84c6b98a3316a7c1d4f02cc4d7fc91ffbe163
mcepulis <109752570+mcepulis@users.noreply.github.com>
2024-03-13 08:29:17 +0200

    commit my nd

--- middlewares/timeHeader.js
+++ middlewares/timeHeader.js
@@ -1,8 +1,5 @@
 function timeHeader(req, res, next) {
     req.laikas = `${new Date().getHours()}val  ${new Date().getMinutes()}min  ${new Date().getSeconds()}sec`;
-   
-    console.log(req.laikas);
-
     next();
 }
 
